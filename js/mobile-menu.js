a93db0389c663ab5a3e772874e6eedfa1f9430b2
danger_videograph <nikolastartsev@gmail.com>
2022-06-13 22:36:56 +0300

    edit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const logo = document.querySelector(".header__logo")
+  const hr_header = document.querySelector(".header hr")
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -12,6 +13,7 @@
 
     logo.classList.toggle("is-open");
     document.body.classList.toggle("modal-body-open");
+    hr_header.classList.toggle("is-open");
 
     document.querySelector(".header__head").classList.toggle("flex-end");
 
